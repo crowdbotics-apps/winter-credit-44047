6b2a2b127aa224df8394de835f17cd5f11cce82f
Crowdbotics <team@crowdbotics.com>
2023-10-19 18:37:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-credit-44047",
+  url: "https://winter-credit-44047.botics.co",
+  api: "https://winter-credit-44047.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
